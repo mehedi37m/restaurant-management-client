e33f29b784b3ea9484571b54f0f9d5e30d455cd6
mehedi37m <mz.mehedi37@gmail.com>
2023-11-07 18:36:30 +0600

    add search

--- src/Page/AllFood/AllFoodCard.jsx
+++ src/Page/AllFood/AllFoodCard.jsx
@@ -4,6 +4,7 @@ import Cards from './Cards';
 const AllFoodCard = () => {
 
 const [foodCards, setFoodCards] = useState([]);
+const [searchFood, setSearchFood] = useState('');
 
 useEffect(() =>{
     fetch('http://localhost:5000/items')
@@ -11,12 +12,26 @@ useEffect(() =>{
     .then(data => setFoodCards(data))
 },[])
 
-console.log(foodCards)
+// console.log(foodCards)
 
+
+const handleSearch = () => {
+    fetch(`http://localhost:5000/SearchFood/${searchFood}`)
+    .then(res => res.json())
+    .then(data => {
+        setFoodCards(data)
+    })
+
+}
 
     return (
         <div className='my-10'>
             <h1 className='text-5xl text-center font-bold'>All Food Here...</h1>
+
+            <div>
+                <input onChange={(e)=>setSearchFood(e.target.value)} type="text" placeholder='search food name' name="name" id="" />
+                <button onClick={handleSearch}>Search</button>
+            </div>
 
             <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 justify-items-center'>
                 {
